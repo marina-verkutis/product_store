8c2c9fc0db350015d477d033f51b2d4acd59d75f
Marina Shisherina <cognitive.mmm@yandex.ru>
2020-06-27 19:34:42 +0300

    first commit

---
+++ src/main/java/com/vivistore/store/controllers/StoreController.java
@@ -0,0 +1,16 @@
+package com.vivistore.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StoreController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "index";
+    }
+
+}
